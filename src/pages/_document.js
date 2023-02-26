7adf5f98d1ea836bc0acd4666e945765fc2cf242
gom20 <rhaldud89@gmail.com>
2023-02-26 15:38:04 +0900

    modified commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,13 +4,11 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@400;500;600;700;900&display=swap" rel="stylesheet" />
                 <link
                     rel="stylesheet"
                     type="text/css"
-                    charset="UTF-8"
+                    charSet="UTF-8"
                     href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
                 />
                 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
